5655dc615a23d27a8435f5d198d5deae471f3aef
ghetiaalish2602@gmail.com <ghetiaalish2602@gmail.com>
2024-09-11 18:18:56 +0530

    first comnit

---
+++ main.js
@@ -0,0 +1,9 @@
+const icon=document.getElementById("m-icon");
+const navlinks=document.getElementById("navlinks");
+
+icon.addEventListener("click",()=>{
+
+    navlinks.classList.toggle("navlinks-active");
+})
+
+
---
+++ u.js
@@ -0,0 +1,4 @@
+
+var json = '{ "firstName":"ASHU", "lastName":"BHATI", "studentCode":7 }';
+var student = JSON.parse(json);
+console.log(student.firstName + " " + student.lastName);
